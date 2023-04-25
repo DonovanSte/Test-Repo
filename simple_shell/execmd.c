69d60e14c738c50584a3cc0d2299a188c9abc3df
Staz86 <charles.diale13@gmail.com>
2023-04-25 23:49:51 +0200

    split main functions

--- simple_shell/execmd.c
+++ simple_shell/execmd.c
@@ -2,38 +2,38 @@
 
 void execmd(char **argv)
 {
-    char *command = NULL, *actual_command = NULL;
-    pid_t pid;
-    int status;
+	char *command = NULL, *actual_command = NULL;
+	pid_t pid;
+	int status;
 
-    if (argv)
-    {
-        command = argv[0];
-        actual_command = get_location(command);
+	if (argv)
+	{
+		command = argv[0];
+		actual_command = get_location(command);
 
-        if (actual_command == NULL)
-        {
-            printf("Command not found: %s\n", command);
-            return;
-        }
+		if (actual_command == NULL)
+		{
+			printf("Command not found: %s\n", command);
+			return;
+		}
 
-        pid = fork();
-        if (pid < 0)
-        {
-            perror("Error");
-            exit(EXIT_FAILURE);
-        }
-        else if (pid == 0)
-        {
-            if (execve(actual_command, argv, NULL) == -1)
-            {
-                perror("Error:");
-            }
-            exit(EXIT_FAILURE);
-        }
-        else
-        {
-            wait(&status);
-        }
-    }
+		pid = fork();
+		if (pid < 0)
+		{
+			perror("Error");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+		{
+			if (execve(actual_command, argv, NULL) == -1)
+			{
+				perror("Error:");
+			}
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
 }
--- simple_shell/functions.c
+++ simple_shell/functions.c
@@ -2,87 +2,87 @@
 
 extern char **environ;
 
-void print_prompt(char *prompt) 
+void print_prompt(char *prompt)
 {
-    printf("%s", prompt);
+	printf("%s", prompt);
 }
 
 char* read_line(size_t *n)
 {
-    ssize_t nread;
-    char *lineptr = NULL;
+	ssize_t nread;
+	char *lineptr = NULL;
 
-    nread = getline(&lineptr, n, stdin);
+	nread = getline(&lineptr, n, stdin);
 
-    if (nread == -1) 
-    {
-        return NULL;
-    }
+	if (nread == -1)
+	{
+		return NULL;
+	}
 
-    return lineptr;
+	return lineptr;
 }
 
 int execute_command(char **argv)
 {
-    if (strcmp(argv[0], "exit") == 0)
-    {
-        exit(EXIT_SUCCESS);
-    } 
-    else if (strcmp(argv[0], "env") == 0)
-    {
-        char **env = environ;
-        while (*env)
-        {
-            printf("%s\n", *env++);
-        }
-        return 1;
-    } 
-    else 
-    {
-        execmd(argv);
-        return 1;
-    }
+	if (strcmp(argv[0], "exit") == 0)
+	{
+		exit(EXIT_SUCCESS);
+	}
+	else if (strcmp(argv[0], "env") == 0)
+	{
+		char **env = environ;
+		while (*env)
+		{
+			printf("%s\n", *env++);
+		}
+		return 1;
+	}
+	else
+	{
+		execmd(argv);
+		return 1;
+	}
 }
 
-int tokenize_line(char *lineptr, char ***argv) 
+int tokenize_line(char *lineptr, char ***argv)
 {
-    char *lineptr_cpy = NULL;
-    const char *delim = " \n";
-    int token_num = 0;
-    char *token;
-    int i;
+	char *lineptr_cpy = NULL;
+	const char *delim = " \n";
+	int token_num = 0;
+	char *token;
+	int i;
 
-    lineptr_cpy = malloc(sizeof(char) * strlen(lineptr));
-    if (lineptr_cpy == NULL) {
-        perror("memory allocation error");
-        return -1;
-    }
+	lineptr_cpy = malloc(sizeof(char) * strlen(lineptr));
+	if (lineptr_cpy == NULL) {
+		perror("memory allocation error");
+		return -1;
+	}
 
-    strcpy(lineptr_cpy, lineptr);
+	strcpy(lineptr_cpy, lineptr);
 
-    token = strtok(lineptr, delim);
+	token = strtok(lineptr, delim);
 
-    while (token != NULL) 
-    {
-        token_num++;
-        token = strtok(NULL, delim);
-    }
-    token_num++;
+	while (token != NULL)
+	{
+		token_num++;
+		token = strtok(NULL, delim);
+	}
+	token_num++;
 
-    *argv = malloc(sizeof(char *) * token_num);
+	*argv = malloc(sizeof(char *) * token_num);
 
-    token = strtok(lineptr_cpy, delim);
+	token = strtok(lineptr_cpy, delim);
 
-    for (i = 0; token != NULL; i++) 
-    {
-        (*argv)[i] = malloc(sizeof(char) * strlen(token));
-        strcpy((*argv)[i], token);
+	for (i = 0; token != NULL; i++)
+	{
+		(*argv)[i] = malloc(sizeof(char) * strlen(token));
+		strcpy((*argv)[i], token);
 
-        token = strtok(NULL, delim);
-    }
-    (*argv)[i] = NULL;
+		token = strtok(NULL, delim);
+	}
+	(*argv)[i] = NULL;
 
-    free(lineptr_cpy);
+	free(lineptr_cpy);
 
-    return token_num;
+	return token_num;
 }
